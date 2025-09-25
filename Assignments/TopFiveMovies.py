41f51de583b4958d9c0d41817780679ff146d344
Naegle Tyler <821395@stma.k12.mn.us>
2025-09-25 13:15:13 -0500

    update

--- Assignments/TopFiveMovies.py
+++ Assignments/TopFiveMovies.py
@@ -1,1 +1,7 @@
 #assignment
+print ("Top Five Moveis")
+print ("Star Wars")
+print ("Cars 2")
+print ("rouge one")
+print ("Lorax")
+print ("cruds")
---
+++ Practice/2.3-practice.py
@@ -0,0 +1,27 @@
+# String    - "in quotations"
+# Integer   - a whole number ex. 1 4 -10 ect.
+# Float     - a decimal nimber ex 3.1415765989
+# Char      - a single glyph ex.  'c'
+# BooL      - True or False
+
+# How to define a variable!!
+# <name> = <value>
+
+lowercase = False
+UPPERCASE = False
+UpperCamelCase = False   #ALL lowercase, no spaces, capital for new words
+lowerCamelCase = False   #ALL lowercase, no spaces, capital for new words
+snake_case = True        #All lowercase, underscores for spaces
+TrueSCREAMING_SNAKE_CASE = False
+
+# Other general rules to naming things
+# 1. Concise
+#  -
+
+the_font_size_for_paragrahps = 10
+font_size_para = 10
+para_size = 10
+para = 10
+
+
+
