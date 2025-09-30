5c3ca7bcf765753c15a1e7e3b0a95ed0066ba191
Naegle Tyler <821395@stma.k12.mn.us>
2025-09-30 14:12:22 -0500

    update

--- Assignments/MadLib.py
+++ Assignments/MadLib.py
@@ -23,22 +23,12 @@ adjective = input(adjective)
 
 
 
-noun_place = input("once upon a time, in a [noun] ") 
-print(noun_place)
-adjective = input(" [adjective] [adjective] away. ")
-print(adjective)
-noun_group1 = input("A war raged on between [noun] ")
-print(noun_group1)
-noun_group2 = input("and [noun]. ") 
-print(noun_group2)
-noun_place = input("The two groups saw diffrent vales in the [noun]. ")
-print(noun_place)
-noun_thing = input("One wannted [noun] ")
-print(noun_thing)
-nount_thing2 = input ("and the other wanted [noun]. ")
-print(nount_thing2)
-noun_group = input("both groups clashed for what theu wanted but only one [noun] can rule all. ")
-print(noun_group)
+noun_place = input("gimme a for a place [noun] \n> ") 
+adjective = input(" give me a [adjective] to discribe the last noun \n> ")
+noun_group1 = input("give me [noun] for a group \n> ")
+noun_group2 = input(" give me a [noun] for a second group \n> ") 
+noun_thing = input(" gimme a [noun] for a thing that group 1 wants \n> ")
+nount_thing2 = input (" gimme a [noun] for a thing that group 2 wants \n> ") 
 
 print("once upon a time, in a " + noun_place + adjective + adjective + "away. A war raged on between " + noun_group1 + "and " + noun_group2 + "The two groups saw diffrent vales in the " + noun_place ". " "one wanted")
 
---
+++ Practice/2.6-functions.py
@@ -0,0 +1,17 @@
+def greet():
+     # indented lins are inside the fucntion
+    print("Hello World!")
+    print("This is the second line!!")
+    print("This is the third line!!!!")
+#the function ends here
+print("Hmmm...") # not indented -> no in function!
+
+
+greet()
+ 
+#Create a fucntion that adds two numbers togeather
+def add(x, y):
+    print(x+y)
+
+
+add(10,30)
