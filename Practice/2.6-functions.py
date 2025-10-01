ee3abc87c3239d6de7f59db0a7556303164eeabe
Naegle Tyler <821395@stma.k12.mn.us>
2025-10-01 14:07:16 -0500

    update

--- Practice/2.6-functions.py
+++ Practice/2.6-functions.py
@@ -15,3 +15,48 @@ def add(x, y):
 
 
 add(10,30)
+
+def calculate_tax(iteam, price, rate):
+    print("The amoun of tax to be colected on a " + str(price) + "dollar" +iteam + " is " + str(.1*price*rate))
+
+calculate_tax("Starbucks", 7.99, 0.6825)
+
+def add_five_numbers(a, b, c, d, e):
+    print((a) + (b) + (c) + d + e)
+
+add_five_numbers(5, 10, 30, 40, 5)
+
+
+def full_name(first, last):
+    print(first + " " +last)
+   
+    
+
+first_name = input("enter your first name\n> ")
+last_name = input("enter your last name\n> ")
+
+full_name(first_name, last_name)
+
+
+def area_calculator(length, width):
+    print(length*width)
+
+area_calculator(35, 40)
+area_calculator(16, 27)
+area_calculator(15, 98)
+
+def word_smash(a, b):
+    print(str(a) + str(b))
+
+word_smash("cat", "dog")
+
+
+
+def echo(x, y):
+    print(str(x) * y)
+
+echo("bob", 7)
+
+def happy_birthday(name):
+    print("Happy Birthday to " + name + " Happy Birthday to " + name + " Happy bithday to " + name + " Happy Birthday tooooo " + name)
+happy_birthday("Tyler")
---
+++ Practice/2.7-Reurn and Scope.py
@@ -0,0 +1,22 @@
+def square(number):
+    return number * number
+
+result = square(5)
+print(result) # Outputs: 25
+
+
+gloabl_var = 10 # Global varible
+
+def print_global():
+    print(gloabl_var) # Accessing global varible
+
+print_global() # Outputs: 10 
+
+
+def my_function():
+    local_var = 5 # Local variable
+    print(local_var) # Accessing the local variable
+
+my_function() #outputs 5
+print(local_var) # Causes an error because the local variable does not exist at the global scope
+
