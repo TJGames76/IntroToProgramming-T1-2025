2de9533c7031a8c86bc901cd6acbc7301315ddfe
Naegle Tyler <821395@stma.k12.mn.us>
2025-10-07 14:12:14 -0500

    update

---
+++ Assignments/Boolean Expressions Quiz.py
@@ -0,0 +1,19 @@
+#quiz
+
+answer_1 = int(input("give a number greater then 10.\n> "))
+print(answer_1 > 10)
+
+answer_2 = int(input("pick the number equal to ten\n> "))
+print(answer_2 == 10)
+
+answer_3 = int(input("What is 6 + 7\n> "))
+print(answer_3 == 13)
+
+answer_4 = int(input("what is 6 - 7\n> "))
+print(answer_4 == -1)
+
+answer_5 = int(input("pick a number less then 5\n> "))
+print(answer_5 < 5)
+
+answer_6 = int(input("pick a number greater then 10 but less then 20\n> "))
+print(answer_6 > 10 and answer_6 < 20)
---
+++ Practice/3.1- Boolean Expressions & Operators.py
@@ -0,0 +1,14 @@
+'''
+Equal to (==): Cheacks if two operands are equal
+Greater than(>): Cheacks if to operands are greater then one another
+Same for less then or equal and less then
+
+AND(and): Returns true if both operands are true
+Or(or): Returns true if at least one of the operands is true
+Not(not): Reverses the logical state of its operand 
+
+'''
+
+print(5 > 10)
+
+
