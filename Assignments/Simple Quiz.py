bcd7f7a3db68c6dfa627f6830804b443aa8ec6c1
Naegle Tyler <821395@stma.k12.mn.us>
2025-10-09 13:15:42 -0500

    update

--- Assignments/Simple Quiz.py
+++ Assignments/Simple Quiz.py
@@ -7,31 +7,43 @@ quetion_2 = input("What is 8 * 8\n> ")
 answer_3 = "100"
 quetion_3 = input("What is 10 squared\n> ")
 
-answer_4 = -5
+answer_4 = "-5"
 quetion_4 = input("What is 5 - 10\n> ")
 
-answer_5 = 5
+answer_5 = "5"
 quetion_5 = input("What is 25 / 5\n> ")
 
 def tally_score():
     if quetion_1 == answer_1:
         print("Correct")
+        a = 1
     else:
         print("Incorrect")
+        a = 0
     if quetion_2 == answer_2:
         print("Correct")
+        b = 1
     else:
-        print("Incorrect")
+        print("Incorrect") 
+        b = 0
     if quetion_3 == answer_3:
         print("Correct")
+        c = 1
     else:
         print("Incorrect")
+        c = 0
     if quetion_4 == answer_4:
         print("Corrcet")
+        d = 1
     else:
         print("Incorrect")
+        d = 0
     if quetion_5 == answer_5:
         print("Correct")
+        e = 1
     else:
         print("Incorrect")
+        e = 0
+    print("Score =")
+    print(a + b + c + d + e)
 tally_score()
