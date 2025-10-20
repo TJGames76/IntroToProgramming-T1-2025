4859332d808b10802c164e69b5983d4384afc62e
Naegle Tyler <821395@stma.k12.mn.us>
2025-10-20 14:06:19 -0500

    Update

--- Assignments/Fortune Teller.py
+++ Assignments/Fortune Teller.py
@@ -3,7 +3,7 @@ def fourtune_teller():
     print("Hi I am the fortune teller and i will take a imput of intgers to tell your fortune")
     luck = 0
     Lucky_number = input("Enter your lucky number as a integer\n> ")
-    if Lucky_number == random.randint(0, 100):
+    if int(Lucky_number) >= 50:
         luck = luck - 1
     else:
         luck = luck + 1
